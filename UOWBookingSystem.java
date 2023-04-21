d9e461765cd54d7b2d424fc447715ae133be0593
mc <zacknight423@gmail.com>
2023-04-21 10:44:23 +0800

    user class

--- UOWBookingSystem.java
+++ UOWBookingSystem.java
@@ -431,3 +431,44 @@ class Room {
                 pricing, timing, capacity, booked);
     }
 }
+
+class User{
+    String username;
+    String password;
+    Boolean isStaff;
+
+    public User(){
+        isStaff = false;
+    }
+
+    public User(String username, String password, Boolean isStaff) {
+        this.username = username;
+        this.password = password;
+        this.isStaff = isStaff;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public Boolean isStaff(){
+        return isStaff;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setIsStaff(Boolean isStaff){
+        this.isStaff = isStaff;
+    }
+}
+
